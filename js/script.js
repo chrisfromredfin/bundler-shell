1731595877d8f3e06fdbebec0dada7f7937c0807
Christina Costello <christina@redfinsolutions.com>
2017-03-29 14:21:51 -0400

    shell for drupal 8

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * Global JavaScript.
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Drupal attach behavior.
+   */
+  Drupal.behaviors.site = {
+    attach: function (context, settings) {
+    },
+    detach: function (context, settings) {
+    }
+  };
+
+})(jQuery, Drupal);
